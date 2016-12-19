1f882c60b548d6c37026d013c5fe89158985d23e
simaski <jimmyhernandezu@gmail.com>
2016-12-19 09:36:39 -0300

    Initial commit

---
+++ app/src/main/java/com/rinno/simaski/bluetoothaplication/MainActivity.java
@@ -0,0 +1,40 @@
+package com.rinno.simaski.bluetoothaplication;
+
+import android.app.AlertDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    BluetoothAdapter BTAdapter;
+
+    public static int REQUEST_BLUETOOTH = 1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BTAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (BTAdapter == null) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Not compatible")
+                    .setMessage("Your phone does not support Bluetooth")
+                    .setPositiveButton("Exit", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int which) {
+                            System.exit(0);
+                        }
+                    })
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .show();
+        }else   if (!BTAdapter.isEnabled()) {
+            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, REQUEST_BLUETOOTH);
+        }
+
+
+    }
+}
